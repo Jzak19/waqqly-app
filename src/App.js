4ac3a8a9ff9771bb3a09874189a8f2134b47fa30
Jakub Zak <j.zak19@unimail.winchester.ac.uk>
2024-02-23 18:11:36 +0000

    Added user profile page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from './components/pages/homePage.js'
 import OwnerRegister from './components/pages/ownerRegister.js'
 import WalkerRegister from './components/pages/walkerRegister.js';
 import SignIn from './components/pages/signIn.js'
+import UserPage from './components/pages/userPage.js';
 import { BrowserRouter as Router, Route, Routes, NavLink} from 'react-router-dom';
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
           <Route path='/ownerRegister' exact element={<OwnerRegister />} />
           <Route path='/walkerRegister' exact element={<WalkerRegister />} />
           <Route path='/signIn' exact element = {<SignIn />} />
+          <Route path='/userProfile' exact element={<UserPage/>}/>
         </Routes>
      
     </Router>
